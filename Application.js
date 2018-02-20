32264b25381795b16b23e8d4fdd98e00f2a72e77
Ryan <ryan@happenweb.com>
2018-02-20 16:55:26 +0000

    Backend framebuffer added to provide feedback for clicks (not in working order)

--- Application.js
+++ Application.js
@@ -6,15 +6,36 @@
         }
 
         proto.landscape = landscape();
+        proto.commands = commands();
 
         return proto;
+    }
+
+    function commands(){
+
+        let proto = {
+            landscapeClick: {
+                pending: false,
+                x: null,
+                y: null
+            },
+            clickOnLandscape: function(x, y){
+                this.landscapeClick = {
+                    pending: true,
+                    x: x,
+                    y: y
+                }
+            }
+        }
+
+        return Object.create(proto);
     }
 
     function landscape(){
 
         let proto = {
             gameSpace:[],
-            xSize: 30,
+            xSize: 60,
             ySize: 30,
             zSize: 5,
             forEach: function(action){
@@ -26,6 +47,7 @@
                     }
                 }
             }
+
         }
 
         function initGameSpace(){
@@ -40,9 +62,6 @@
                         if(!proto.gameSpace[x][y]){
                             proto.gameSpace[x][y] = [];
                         }
-                        if(!proto.gameSpace[x][y][z]){
-                            proto.gameSpace[x][y][z] = [];
-                        }
 
                         proto.gameSpace[x][y][z] = tileGenerationRule(x,y,z);
                     }    
@@ -54,9 +73,19 @@
             if(z == 0){
                 return tile(true);
             }
-            // if(z == 1 && x == 2){
-            //     return tile(true);
-            // }
+            if(z == 1 && Math.random() < 0.5){
+                return tile(true);
+            }
+            if(z == 2 && Math.random() < 0.5){
+                if(proto.gameSpace[x,y,z-1] && proto.gameSpace[x,y,z-1].visible){
+                    return tile(true);
+                }
+            }
+            if(z == 3 && Math.random() < 0.25){
+                if(proto.gameSpace[x,y,z-1] && proto.gameSpace[x,y,z-1].visible){
+                    return tile(true);
+                }
+            }
             return tile(false);
         }
 
@@ -98,6 +127,11 @@ function scrollUpHandler(e) {
         }
         return true;
     }
+
+    function clickhandler(e){
+        // gameState.commands.clickOnLandscape(e.clientX, e.clientY);
+        GetObjectFromFrameBuffer(e.clientX, e.clientY);
+    }        
 
     function keyPressHandler(e){
         console.log(e.key);
@@ -153,6 +187,14 @@ function scrollUpHandler(e) {
     
     let program = createProgram(gl,  vertexShader, fragmentShader);
 
+    let objectPickerVertexShaderSource = document.getElementById("object-picking-vertex-shader").text;
+    let objectPickerFragmentShaderSource = document.getElementById("object-picking-fragment-shader").text;
+
+    let objectPickerVertexShader = createShader(gl, gl.VERTEX_SHADER, objectPickerVertexShaderSource);
+    let objectPickerFragmentShader = createShader(gl, gl.FRAGMENT_SHADER, objectPickerFragmentShaderSource);
+    
+    let objectPickerProgram = createProgram(gl,  objectPickerVertexShader, objectPickerFragmentShader);
+
     var zIndex = 30;
     var xScroll = -20;
     var yScroll = -20;
@@ -160,6 +202,7 @@ function scrollUpHandler(e) {
     document.addEventListener('wheel', scrollUpHandler);
     document.addEventListener('keydown', keyPressHandler);
     document.addEventListener('keyup', keyUpHandler);
+    document.addEventListener('click', clickhandler);
 
     var upArrowPressed = false;
     var downArrowPressed = false;
@@ -411,12 +454,9 @@ function scrollUpHandler(e) {
 
     let worldMatrix = new Float32Array(16);
     let viewMatrix = new Float32Array(16);
-    let projMatrix = new Float32Array(16);
-    mat4.identity(worldMatrix);
-    mat4.perspective(projMatrix, glMatrix.toRadian(45), canvas.width / canvas.height, 0.1, 1000.0);
+
 
     gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
-    gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
 
     // let xRotationMatrix = new Float32Array(16);
     // let yRotationMatrix = new Float32Array(16);
@@ -431,37 +471,63 @@ function scrollUpHandler(e) {
     let shortDiameter = Math.sqrt(3) * longRadius;
     let shortRadius = shortDiameter / 2;
 
-    let scrollRate = 0.2;
+    let scrollRate = 20;
+
+    var width = canvas.width;
+    var height = canvas.height;
+    var pixels = new Uint8Array(4 * 1);
 
     //
     // Main render loop
     //
-
+    var then = 0;
     var angle = 0;
-    var loop = function () {
+    var loop = function (now) {
         resize(canvas);
+        gl.useProgram(program);
+
+            let projMatrix = new Float32Array(16);
+    mat4.identity(worldMatrix);
+    mat4.perspective(projMatrix, glMatrix.toRadian(45), canvas.width / canvas.height, 0.1, 1000.0);
+    gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
+    
+        // //CLick
+        // if(gameState.commands.landscapeClick.pending){
+        //     gameState.commands.landscapeClick.pending = false;
+
+        //     var pixels = new Uint8Array(gl.drawingBufferWidth * gl.drawingBufferHeight * 4);
+
+        //     gl.readPixels(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight, gl.RGBA, gl.UNSIGNED_BYTE, pixels);
+
+        //     console.log(pixels)
+        // }
+
+        // Convert the time to seconds
+        now *= 0.001;
+        // Subtract the previous time from the current time
+        var deltaTime = now - then;
+        // Remember the current time for the next frame.
+        then = now;
 
         if(upArrowPressed){
-            yScroll += scrollRate;
-            xScroll += scrollRate;
+            yScroll += scrollRate * deltaTime;
+            xScroll += scrollRate * deltaTime;
         }
         if(leftArrowPressed){
-            yScroll += scrollRate;
-            xScroll -= scrollRate;
+            yScroll += scrollRate * deltaTime;
+            xScroll -= scrollRate * deltaTime;
         }
         if(rightArrowPressed){
-            yScroll -= scrollRate;
-            xScroll += scrollRate;
+            yScroll -= scrollRate * deltaTime;
+            xScroll += scrollRate * deltaTime;
         }
         if(downArrowPressed){
-            yScroll -= scrollRate;
-            xScroll -= scrollRate;
+            yScroll -= scrollRate * deltaTime;
+            xScroll -= scrollRate * deltaTime;
         }
 
-            // set the light direction.
-    gl.uniform3fv(reverseLightDirectionLocation, [0.5, 1, 1.2]);
-
-        gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
+        // set the light direction.
+        gl.uniform3fv(reverseLightDirectionLocation, [0.5, 1, 1.2]);
 
         mat4.lookAt(viewMatrix, [xScroll, yScroll, zIndex], [xScroll + zIndex, yScroll + zIndex, 0], [0, 0, 1]);
         gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
@@ -469,7 +535,7 @@ function scrollUpHandler(e) {
         gl.clearColor(0.75, 0.85, 0.8, 1.0);
         gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
 
-        var identityMatrix = new Float32Array(16);
+        var identityMatrix = new Float32Array(16);        
 
         gameState.landscape.forEach(function(x, y, z){
             if(gameState.landscape.gameSpace[x][y][z].visible){
@@ -492,6 +558,102 @@ function scrollUpHandler(e) {
         requestAnimationFrame(loop);
     };
     requestAnimationFrame(loop);
+
+    function GetObjectFromFrameBuffer(x, y){
+        gl.useProgram(objectPickerProgram);
+        resize(canvas);
+        // const fb = gl.createFramebuffer();
+        // gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
+        // gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+
+        // // create renderbuffer
+        // depthBuffer = gl.createRenderbuffer();
+        // gl.bindRenderbuffer(gl.RENDERBUFFER, depthBuffer);
+
+        // // allocate renderbuffer
+        // gl.renderbufferStorage(
+        //       gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, gl.drawingBufferWidth, gl.drawingBufferHeight);  
+
+        // // attach renderebuffer
+        // gl.framebufferRenderbuffer(
+        //       gl.FRAMEBUFFER,
+        //       gl.DEPTH_ATTACHMENT,
+        //       gl.RENDERBUFFER,
+        //       depthBuffer);    
+
+        // if (gl.checkFramebufferStatus(gl.FRAMEBUFFER) != gl.FRAMEBUFFER_COMPLETE) {
+        //     alert("this combination of attachments does not work");
+        //     return;
+        // }        
+
+
+        let matWorldUniformLocation = gl.getUniformLocation(objectPickerProgram, 'mWorld');
+        let matViewUniformLocation = gl.getUniformLocation(objectPickerProgram, 'mView');
+        let matProjUniformLocation = gl.getUniformLocation(objectPickerProgram, 'mProj');
+
+        let worldMatrix = new Float32Array(16);
+        let viewMatrix = new Float32Array(16);
+        let projMatrix = new Float32Array(16);
+        mat4.perspective(projMatrix, glMatrix.toRadian(45), canvas.width / canvas.height, 0.1, 1000.0);
+        gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
+        gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
+
+        mat4.lookAt(viewMatrix, [xScroll, yScroll, zIndex], [xScroll + zIndex, yScroll + zIndex, 0], [0, 0, 1]);
+        gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
+        gl.clearColor(0.75, 0.85, 0.8, 1.0);
+        gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
+
+        let hexVertexBufferObject = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, hexVertexBufferObject);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(hexagonVertices), gl.STATIC_DRAW);
+
+        let positionAttribLocation = gl.getAttribLocation(objectPickerProgram, 'vertPosition');
+        let vertColorUniformLocation = gl.getUniformLocation(objectPickerProgram, 'vertColor');
+
+        gl.vertexAttribPointer(
+            positionAttribLocation, // Attribute location
+            3, // Number of elements per attribute
+            gl.FLOAT, // Type of elements
+            gl.FALSE,
+            9 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+            0 // Offset from the beginning of a single vertex to this attribute
+        );
+
+        var identityMatrix = new Float32Array(16);       
+
+
+        let count = 0;
+        gameState.landscape.forEach(function(){
+
+        })
+
+        gameState.landscape.forEach(function(x, y, z){
+            if(gameState.landscape.gameSpace[x][y][z].visible){
+                let xVal = x * (0.75 * longDiameter);
+                let yVal = y * shortDiameter;
+                let zVal = z * longRadius;
+
+                if(x % 2 === 0){
+                    yVal = yVal + shortRadius;
+                }
+
+                let vector = [xVal, yVal, zVal];
+                mat4.identity(identityMatrix);
+                mat4.translate(worldMatrix, identityMatrix, vector);
+                gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
+                gl.uniform3fv(vertColorUniformLocation, [x / gameState.landscape.xSize, y / gameState.landscape.ySize, z / gameState.landscape.zSize]);
+                gl.drawArrays(gl.TRIANGLES, 0, hexagonVertices.length / 9);
+            }
+        });
+
+        var colorPicked = new Uint8Array(4);
+        gl.readPixels(x, y, 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, colorPicked);
+
+        console.log(colorPicked);
+
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null);  
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);        
+    }
 
     function createShader(gl, type, source) {
         let shader = gl.createShader(type);
